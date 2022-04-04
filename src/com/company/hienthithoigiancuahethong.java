f39c1f6d8f1448df4f14b98d2023766658258c9c
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-04-04 14:15:47 +0700

    .

---
+++ src/com/company/hienthithoigiancuahethong.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Date;
+
+public class hienthithoigiancuahethong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
