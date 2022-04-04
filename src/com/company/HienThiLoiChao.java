239f538809db0a8d07240a2b7cddf640ce63072d
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-04-04 17:01:23 +0700

    .

---
+++ src/com/company/HienThiLoiChao.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
