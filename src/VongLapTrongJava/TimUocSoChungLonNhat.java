340f4f856d634e18fabd70a475289dea9a9f51c8
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-04-04 20:46:37 +0700

    .

---
+++ src/VongLapTrongJava/TimUocSoChungLonNhat.java
@@ -0,0 +1,31 @@
+package VongLapTrongJava;
+
+import java.util.Scanner;
+
+public class TimUocSoChungLonNhat {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = sc.nextInt();
+
+        System.out.println("Enter b: ");
+        b = sc.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+            System.out.println("Greatest common factor: " + a);
+        }
+    }
+}
