64c2184bd0712fb51148a04f1f761a36500c7a93
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-04-07 10:03:19 +0700

    .

---
+++ src/OOPLopVaDoiTuongTrongJAVA/HinhChuNhat.java
@@ -0,0 +1,49 @@
+package OOPLopVaDoiTuongTrongJAVA;
+
+
+import java.util.Scanner;
+
+public class HinhChuNhat {
+    double wight;
+    double height;
+
+    public HinhChuNhat(double wight, double height) {
+        this.wight = wight;
+        this.height = height;
+    }
+
+    public HinhChuNhat() {
+    }
+
+    public double getWight() {
+        return wight;
+    }
+
+    public void setWight(double wight) {
+        this.wight = wight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public static double DienTich(double height, double wight) {
+        return (height*wight);
+    }
+
+    public static void main(String[] args) {
+        Scanner Hcn= new Scanner(System.in);
+        System.out.println();
+        System.out.println("Nhập chiều dài");
+        double Height= Hcn.nextDouble();
+        System.out.println("Nhập chiều rộng");
+        double Wight=Hcn.nextDouble();
+        System.out.println("Diện tích là");
+        System.out.println(HinhChuNhat.DienTich((double) Height, (double) Wight));
+    }
+}
+
