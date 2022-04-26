d61bba6942573fecd2f185a1bed62eab4aa8907e
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-04-26 17:16:49 +0700

    .

--- src/text_file/bai_tap/doc_file_csv/TestFile_csv.java
+++ src/text_file/bai_tap/doc_file_csv/TestFile_csv.java
@@ -1,10 +1,21 @@
 package text_file.bai_tap.doc_file_csv;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TestFile_csv {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src\\text_file\\bai_tap\\doc_file_csv\\file.csv");
+        Scanner sc = new Scanner(file);
+        String[] arr;
+        while (sc.hasNext()){
+            arr = sc.nextLine().split(",");
+            for (String i : arr) {
+                System.out.println(i);
+        }
 
+        }
     }
 }
